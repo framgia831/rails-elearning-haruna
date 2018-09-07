d3a2fc9033f720a6d442ced1a3cd0faebc5eb184
堀口はるな <haruna@horiguchiharuna-no-MacBook-Pro-5.local>
2018-09-07 12:06:47 +0800

    Login, Sign up and User page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,14 +9,20 @@ class UsersController < ApplicationController
 			name: params[:user][:name],
 			email: params[:user][:email],
 			password: params[:user][:password],
-      password_confirmation: params[:user][:password_confirmation]
+      password_confirmation: params[:user][:password_confirmation],
+      image_name: "default_user.png"
 		)
 
 		if @user.save
+			session[:user_id] = @user.id
 			redirect_to user_path(@user)
 		else
 			render "new"
 		end
+	end
+
+	def show
+		@user = User.find_by(id: params[:id])
 	end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ApplicationRecord
 	has_secure_password
+
+	EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+	validates :name, presence: true
+	validates :email, presence: true,
+										format: { with: EMAIL_REGEX },
+										uniqueness: true
+
+	
+
 end
