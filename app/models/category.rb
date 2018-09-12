67d76fc22cfd1e97ba02e5f8fe863bb77ab559c6
堀口はるな <haruna@horiguchiharuna-no-MacBook-Pro-5.local>
2018-09-12 09:03:07 +0800

    admin words

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,8 @@ class Category < ApplicationRecord
 	has_many :lessons
 	has_many :words
 
+	accepts_nested_attributes_for :words
+
 	validates :title, presence: true
 	validates :description, presence: true
 end
--- app/models/word.rb
+++ app/models/word.rb
@@ -1,6 +1,9 @@
 class Word < ApplicationRecord
-	belongs_to :category
+	belongs_to :category, dependent: :destroy
 	
 	has_many :answers
 	has_many :choices
+
+	accepts_nested_attributes_for :choices
+	accepts_nested_attributes_for :answers
 end
