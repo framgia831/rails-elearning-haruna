729aa0e2dc708250b6ecb44857776bf8a1a057d0
saki4567 <42955072+saki4567@users.noreply.github.com>
2018-09-12 11:34:33 +0800

    admin words (#5)
    * admin words
    
    * admin words
    
    * fixed words' bug

---
+++ app/controllers/admin/words_controller.rb
@@ -0,0 +1,70 @@
+class Admin::WordsController < ApplicationController
+	before_action :admin_user
+	include UsersHelper
+
+	def index
+		@category = Category.find(params[:category_id])
+		@words = @category.words.all
+	end
+
+	def new
+		@category = Category.find(params[:category_id])
+		@word = @category.words.build
+	end
+
+	def create
+		@category = Category.find(params[:category_id])
+		@word = @category.words.build(word_params)
+
+		if @word.save
+			flash[:notice] = "Sucessfully created!"
+			redirect_to admin_category_words_path(@category)
+		else
+			render "new"
+		end
+	end
+
+	def show
+		@category = Category.find(params[:category_id])
+		@word = Word.find(params[:id])
+	end
+
+	def edit
+		@category = Category.find(params[:category_id])
+		@word = Word.find(params[:id])
+	end
+
+	def update
+		@category = Category.find(params[:category_id])
+		@word = Word.find(params[:id])
+
+		if @word.update(word_params)
+			flash[:notice] = "Sucessfully saved!"
+			redirect_to admin_category_words_path(@category)
+		else
+			render "edit"
+		end
+	end
+
+	def destroy
+		@category = Category.find(params[:category_id])
+		@word = Word.find(params[:id])
+    @word.destroy
+
+    redirect_to admin_category_words_path
+	end
+
+	private
+		def admin_user
+			redirect_to root_path unless current_user.admin?
+		end
+
+		def word_params
+			params.require(:word).permit(:content)
+		end
+
+		# def choice_params
+		# 	params.require(:choice).permit(:content)
+		# end
+
+end
